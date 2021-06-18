ac796c0329a07c8dca0e3075dc7ab0d3840d9a9b
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-06-18 15:09:15 +0100

    Improved About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,35 @@ const About = () => {
   return (
     <div className='container full-height'>
       <h2>About</h2>
-      <div>
-        <img className='image-cropper' src={image} />
+      <div className='center'>
+        <h5>
+          Created by: <b>Alaeddine Jmila</b>
+        </h5>
+        <div className='center'>
+          <a
+            href='https://github.com/AladinJmila'
+            target='_blank'
+            rel='noreferrer'
+            style={{ color: 'inherit' }}
+          >
+            <i
+              className='fa fa-github-square fa-2x mr-2'
+              aria-hidden='true'
+            ></i>
+          </a>
+
+          <a
+            href='https://www.linkedin.com/in/alaeddine-jmila/'
+            target='_blank'
+            rel='noreferrer'
+            style={{ color: 'inherit' }}
+          >
+            <i
+              className='fa fa-linkedin-square fa-2x ml-2'
+              aria-hidden='true'
+            ></i>
+          </a>
+        </div>
       </div>
     </div>
   )
