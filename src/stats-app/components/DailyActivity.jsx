f397ba4d1b88daf049fca4e173e203535237dea0
Alaeddine Jmila <jmila.aladin@gmail.com>
2021-10-04 18:41:06 +0100

    After saving The newly added Horizontal bar

--- src/stats-app/components/DailyActivity.jsx
+++ src/stats-app/components/DailyActivity.jsx
@@ -45,8 +45,7 @@ const DailyActivity = () => {
       .append('svg')
       .attr('viewBox', [0, 0, width, height])
       .style('display', 'block')
-    // .attr('width', width)
-    // .attr('height', barHeight * data.length)
+  
 
     svg
       .append('g')
@@ -64,46 +63,7 @@ const DailyActivity = () => {
 
     return svg.node()
 
-    // const bar = svg
-    //   .selectAll('svg')
-    //   .data(data)
-    //   .enter()
-    //   .append('g')
-    //   .attr('transform', (d, i) => {
-    //     // console.log(transValue)
-    //     const trans = `translate(${transValue - prevValue}, 0)`
-    //     transValue += scale(d.value)
-    //     // console.log(scale(d.value))
-
-    //     return trans
-    //   })
-    // .attr('transform', (d, i) => `translate(0, ${i * barHeight})`)
-
-    // transValue = 0
-
-    // bar
-    //   .append('rect')
-    //   .attr('width', d => {
-    //     transValue += scale(d.value) - prevValue
-    //     prevValue = scale(d.value)
-    //     return prevValue
-    //   })
-    //   .attr('height', barHeight - 1)
-    //   .attr('fill', d => d.color)
-
-    // transValue = 0
-    // bar
-    //   .append('text')
-    //   .attr('x', d => {
-    //     transValue += d.value * scaleFactor - prevValue
-    //     prevValue = d.value * scaleFactor
-    //     return prevValue / 2
-    //   })
-    //   .attr('y', barHeight / 2)
-    //   .attr('dy', '.35em')
-    //   .text(d => d.value)
-
-    // transValue = 0
+    
   }
 
   return (
@@ -115,46 +75,3 @@ const DailyActivity = () => {
 }
 
 export default DailyActivity
-
-// const width= 600
-// const height = 200
-// const margin = {top: 30, right: 10, bottom: 0, left: 30}
-// const formatValue = x => isNaN(x) ? 'N/A' : x.toLacaleString('en')
-// const formatPercent = d3.format('.1%')
-// const x = d3.scaleLinear()
-//             .range([margin.left, width - margin.right])
-// const y = 0
-// // const y = d3.scaleBand()
-// //             .domain(data?.map(d => d.name))
-// //             .range([margin.top, height - margin.bottom])
-// //             .padding(0.08)
-
-// const XAxis = g => g.attr('transform', `translate(${margin.left},0)`)
-//                     .call(d3.axisTop(x).ticks(width / 100, '%'))
-//                     .call(g => g.selectAll('.domain'.remove()))
-
-// const yAxis = g => g.att('transfrom', `translate(${margin.left},0)`)
-//                     .call(d3.axisLeft(y).tickSizeOuter(0))
-//                     .call(g => g.selectAll('.domain').remove())
-
-// // const color = d3.scaleOrdinal()
-// //                 .domain(data?.map(d => d.key))
-// //                 .range(d3.schemeSpectral[data.length])
-// //                 .unknown('#ccc')
-
-// const chart = () => {
-//     const svg = d3.create('svg')
-//                 .attr('viewBox', [0, 0, width, height])
-//                 .style('overflow', 'visible')
-
-//     svg.append('g')
-//       .selectAll('g')
-//       .data(data)
-//         .enter().append('g')
-//       .attr('fill', d => d.color)
-//       .selectAll('rect')
-//       .data(d => d)
-//       .join('rect')
-//         .attr('x', d => x(d[0]))
-//         .attr('y', (d, i) => y())
-// }
