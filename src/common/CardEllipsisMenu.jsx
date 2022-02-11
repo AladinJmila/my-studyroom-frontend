c5f49d57f304e02dfc959f03c2f08239277efc11
AladinJmila <jmila.aladin@gmail.com>
2022-02-11 12:57:05 +0000

    Cleaned and restyled CardEllipsisMenu component

--- src/common/CardEllipsisMenu.jsx
+++ src/common/CardEllipsisMenu.jsx
@@ -18,97 +18,63 @@ const CardEllipsisMenu = ({
   };
 
   const iconClass = vertical
-    ? 'fa fa-ellipsis-v align-top pointer'
-    : 'fa fa-lg fa-ellipsis-h align-top pointer';
+    ? 'fa fa-ellipsis-v pointer'
+    : 'fa fa-lg fa-ellipsis-h pointer';
 
   return (
     <>
       <i onClick={handleShowMenu} className={iconClass} aria-hidden='true'></i>
       {showMenu && (
-        <div
-          className='list-group float-end'
-          style={{
-            right: 10,
-            minWidth: 100,
-            position: 'absolute',
-            float: 'right',
-            zIndex: 2000,
-          }}
-        >
+        <div className='ellipsis-menu'>
           <div
-            className='list-group-item list-group-item-action pointer'
+            className='ellipsis-menu-item'
             onClick={() => {
               onEdit(item);
               handleShowMenu();
             }}
           >
-            <h6 style={{ margin: 0 }}>
-              Edit
-              <i className='fa fa-pencil float-end' aria-hidden='true'></i>
-            </h6>
+            <h6>Edit</h6>
+            <i className='fa fa-pencil' aria-hidden='true'></i>
           </div>
 
           <div
-            className='list-group-item list-group-item-action pointer'
+            className='ellipsis-menu-item'
             onClick={() => {
               onToggleProp(item, 'starred');
               handleShowMenu();
             }}
           >
-            <h6 style={{ margin: 0 }}>
-              Star
-              <div className='float-end'>
-                <Star className='yellow' starred={item.starred} />
-              </div>
-            </h6>
+            <h6>Star</h6>
+            <Star starred />
           </div>
 
           <div
-            className='list-group-item list-group-item-action pointer'
+            className='ellipsis-menu-item'
             onClick={() => {
               onToggleProp(item, 'isPublic');
               handleShowMenu();
             }}
           >
-            <h6 style={{ margin: 0 }}>
-              Public
-              <div className='float-end'>
-                <Toggle toggled={item.isPublic} />
-              </div>
-            </h6>
+            <h6>Public</h6>
+            <Toggle toggled={item.isPublic} />
           </div>
 
           {share && (
             <div
-              className='list-group-item list-group-item-action pointer'
+              className='ellipsis-menu-item'
               onClick={() => {
                 onShareForm();
                 handleShowMenu();
               }}
             >
-              <h6 style={{ margin: 0 }}>
-                Share
-                <i
-                  className='fa fa-share-alt float-end'
-                  aria-hidden='true'
-                  style={{ marginTop: 2 }}
-                ></i>
-              </h6>
+              <h6>Share</h6>
+              <i className='fa fa-share-alt' aria-hidden='true'></i>
             </div>
           )}
 
-          <div
-            className='list-group-item list-group-item-action pointer'
-            onClick={() => onDelete(item)}
-          >
-            <h6 style={{ margin: 0, color: 'red' }}>
-              Delete
-              <i
-                className='fa fa-trash-o float-end'
-                aria-hidden='true'
-                style={{ marginTop: 2 }}
-              ></i>
-            </h6>
+          <div className='ellipsis-menu-item' onClick={() => onDelete(item)}>
+            <h6>Delete</h6>
+            <i className='fa fa-trash-o' aria-hidden='true'></i>
           </div>
         </div>
       )}
