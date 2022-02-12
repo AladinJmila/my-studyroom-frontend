eae9443da78091b695674f2dd0dc93d12b1a4510
AladinJmila <jmila.aladin@gmail.com>
2022-02-12 12:21:26 +0000

    safe point

--- src/resources-app/components/Resources.jsx
+++ src/resources-app/components/Resources.jsx
@@ -1,69 +1,69 @@
-import _ from 'lodash'
-import { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import HeaderCard from '../../common/HeaderCard'
-import SortCard from '../../common/SortCard'
-import ResourcesForm from './ResourcesForm'
-import ResourcesYoutubeForm from './ResourcesYoutubeForm'
-import ResourcesCard from './ResourcesCard'
-import { BeatLoader } from 'react-spinners'
+import _ from 'lodash';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import HeaderCard from '../../common/HeaderCard';
+import SortCard from '../../common/SortCard';
+import ResourcesForm from './ResourcesForm';
+import ResourcesYoutubeForm from './ResourcesYoutubeForm';
+import ResourcesCard from './ResourcesCard';
+import { BeatLoader } from 'react-spinners';
 import {
   loadResources,
   patchResource,
   deleteResource,
   setSelectedResource,
   toggleResourceProp,
-} from './../../store/apps/resourcesActions'
+} from './../../store/apps/resourcesActions';
 import {
   updateSubjectItemsCount,
   updateSubjectCheckedItemsCount,
-} from './../../store/apps/subjectsActions'
+} from './../../store/apps/subjectsActions';
 
 function Resources() {
-  const [showForm, setShowForm] = useState(false)
-  const [showYoutubeForm, setShowYoutubeForm] = useState(false)
+  const [showForm, setShowForm] = useState(false);
+  const [showYoutubeForm, setShowYoutubeForm] = useState(false);
   const [sortTarget, setSortTarget] = useState({
     path: 'initial',
     order: 'asc',
-  })
+  });
 
-  const dispatch = useDispatch()
-  const resources = useSelector(state => state.apps.resources.list)
-  const { selectedSubject } = useSelector(state => state.apps.subjects)
+  const dispatch = useDispatch();
+  const resources = useSelector(state => state.apps.resources.list);
+  const { selectedSubject } = useSelector(state => state.apps.subjects);
   const numOfResources = useSelector(
     state =>
       state.ui.resourcesPerSubject[`${selectedSubject?.name}`] ||
       state.ui.resourcesPerSubject['All Subjects']
-  )
-  const { user } = useSelector(state => state.auth)
-  const { loading } = useSelector(state => state.apps.resources)
+  );
+  const { user } = useSelector(state => state.auth);
+  const { loading } = useSelector(state => state.apps.resources);
 
   useEffect(() => {
-    dispatch(loadResources())
-  }, [])
+    dispatch(loadResources());
+  }, []);
 
   const handleDelete = resource => {
-    dispatch(deleteResource(resource._id))
-    dispatch(updateSubjectItemsCount(resource, 'Resources', 'delete'))
-  }
+    dispatch(deleteResource(resource._id));
+    dispatch(updateSubjectItemsCount(resource, 'Resources', 'delete'));
+  };
 
   const handleResourceSelect = resource => {
-    dispatch(setSelectedResource(resource))
-    handleShowForm()
-  }
+    dispatch(setSelectedResource(resource));
+    handleShowForm();
+  };
 
   const onSort = sortTarget => {
-    setSortTarget(sortTarget)
-  }
+    setSortTarget(sortTarget);
+  };
 
   const handleToggleProp = (resource, property) => {
-    const index = resources.indexOf(resource)
-    const resourceToUpdate = { ...resources[index] }
-    resourceToUpdate[property] = !resourceToUpdate[property]
-    const update = { [property]: resourceToUpdate[property] }
+    const index = resources.indexOf(resource);
+    const resourceToUpdate = { ...resources[index] };
+    resourceToUpdate[property] = !resourceToUpdate[property];
+    const update = { [property]: resourceToUpdate[property] };
 
-    dispatch(patchResource(resource._id, update))
-    dispatch(toggleResourceProp(resource._id, property))
+    dispatch(patchResource(resource._id, update));
+    dispatch(toggleResourceProp(resource._id, property));
     if (property === 'isChecked')
       dispatch(
         updateSubjectCheckedItemsCount(
@@ -71,26 +71,26 @@ function Resources() {
           'Resources',
           update
         )
-      )
-  }
+      );
+  };
 
   const handleShowForm = () => {
-    setShowForm(showForm ? false : true)
-  }
+    setShowForm(showForm ? false : true);
+  };
 
   const handleShowYoutubeForm = () => {
-    setShowYoutubeForm(showYoutubeForm ? false : true)
-  }
+    setShowYoutubeForm(showYoutubeForm ? false : true);
+  };
 
   const filtered =
     selectedSubject && selectedSubject._id
       ? resources.filter(r => r.subject._id === selectedSubject._id)
-      : resources
+      : resources;
 
-  const sorted = _.orderBy(filtered, [sortTarget.path], [sortTarget.order])
+  const sorted = _.orderBy(filtered, [sortTarget.path], [sortTarget.order]);
 
   return (
-    <>
+    <div className='sub-app-body'>
       <div className='sticky-top'>
         <HeaderCard
           user={user}
@@ -136,8 +136,8 @@ function Resources() {
             ))}
         </>
       )}
-    </>
-  )
+    </div>
+  );
 }
 
-export default Resources
+export default Resources;
